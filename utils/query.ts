37897ef015b3384e28d0bd6167a9a83cd23a4f9d
Husoschi Ionut-Catalin <ionuthusoschi@gmail.com>
2026-01-28 21:32:03 +0200

    revert to prev commit

--- utils/query.ts
+++ utils/query.ts
@@ -3,60 +3,69 @@ import { unstable_noStore as noStore } from "next/cache";
 export async function getQuery(query: string) {
   noStore();
 
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api${query}`,
-  );
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_STRAPI_URL}/api${query}`,
+    );
 
-  if (!response.ok) {
-    return { data: null, error: `HTTP error! status: ${response.status}` };
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    if (!data.data) {
+      throw new Error(data.error?.message || "No data available");
+    }
+
+    return data;
+  } catch (error) {
+    throw error instanceof Error ? error : new Error("Unknown error");
   }
-
-  const result = await response.json();
-
-  if (!result.data) {
-    return { data: null, error: result.error?.message || "No data available" };
-  }
-
-  return { data: result.data, error: null };
 }
 
 export async function postData(query: string, formData: any) {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api${query}`,
-    {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.API_TOKEN_SALT}`,
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_STRAPI_URL}/api${query}`,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.API_TOKEN_SALT}`, // Include the Authorization header
+        },
+        body: JSON.stringify({ data: formData.data }),
       },
-      body: JSON.stringify({ data: formData.data }),
-    },
-  );
+    );
 
-  if (!response.ok) {
-    const errorData = await response.json();
-    console.log("Error data from Strapi:", errorData);
-    return { error: errorData.message || "Network response was not ok" };
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.log("Error data from Strapi:", errorData);
+      throw new Error(errorData.message || "Network response was not ok");
+    }
+  } catch (error) {
+    const err = error as Error;
+    console.warn("Error posting data:", err);
+    return { error: err.message };
   }
-
-  return { error: null };
 }
 
 export async function telegramNotification(message: string) {
-  const encodedMessage = encodeURIComponent(message);
-  const response = await fetch(
-    `https://api.telegram.org/${process.env.TELEGRAM_API_TOKEN}/sendMessage?chat_id=${process.env.TELEGRAM_CHAT_ID}&text=${encodedMessage}`,
-    {
-      method: "POST",
-    },
-  );
+  try {
+    const encodedMessage = encodeURIComponent(message);
+    const response = await fetch(
+      `https://api.telegram.org/${process.env.TELEGRAM_API_TOKEN}/sendMessage?chat_id=${process.env.TELEGRAM_CHAT_ID}&text=${encodedMessage}`,
+      {
+        method: "POST",
+      },
+    );
+    if (!response.ok) {
+      const errorData = await response.json();
 
-  if (!response.ok) {
-    const errorData = await response.json();
-    console.warn("Error sending Telegram notification:", errorData);
-    return { error: errorData.description || "Network response was not ok" };
+      throw new Error(errorData.description || "Network response was not ok");
+    }
+  } catch (error) {
+    const err = error as Error;
+    console.warn("Error sending Telegram notification:", err);
+    return { error: err.message };
   }
-
-  return { error: null };
 }
